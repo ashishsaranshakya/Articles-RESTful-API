fb7b18ddb65f41745cccd3cc96c113edb598c6f5
Ashish Saran Shakya <harshshakya18585@gmail.com>
2023-09-11 18:09:10 +0530

    update database uri

--- model.js
+++ model.js
@@ -1,6 +1,6 @@
 import mongoose from 'mongoose';
 
-mongoose.connect("mongodb+srv://admin-ashish:hotel9ervictor@web-test-projects.bwuoqdk.mongodb.net/todolistDB?retryWrites=true&w=majority", {useNewUrlParser: true, useUnifiedTopology: true});
+mongoose.connect("mongodb+srv://admin-ashish:hotel9ervictor@web-test-projects.bwuoqdk.mongodb.net/articlesDB?retryWrites=true&w=majority", {useNewUrlParser: true, useUnifiedTopology: true});
 
 
 const articleSchema = new mongoose.Schema ({
